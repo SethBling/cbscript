bd334e585ef3840f852ff5c9c98f327b976ce59d
SethBling <sethbling@gmail.com>
2018-11-19 12:42:51 -0800

    Fixed sleeping during the main compile check loop

--- compile.py
+++ compile.py
@@ -4,6 +4,7 @@
 
 import cbscript
 import sys
+import time
 
 if len(sys.argv) <> 2:
 	print "You must include a script filename."
@@ -16,6 +17,6 @@ def run():
 	while True:
 		script.check_for_update()
 			
-	time.sleep(1)
+		time.sleep(1)
 	
 run()
