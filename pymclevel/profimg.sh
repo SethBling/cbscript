7bd85679addd0e9fd136b1ec5cff773f09ea8774
SethBling <sethbling@gmail.com>
2019-01-07 13:52:41 -0800

    Removed pymclevel

--- pymclevel/profimg.sh
+++
@@ -1,1 +0,0 @@
-python gprof2dot.py -f pstats $1 > /tmp/mcedit.dot && dot -Tpng /tmp/mcedit.dot -o $0.png && start $0.png
--- pymclevel/run_nbt_tests.sh
+++
@@ -1,5 +0,0 @@
-rm _nbt.pyd
-py.test test/nbt_test.py
-python setup.py build_ext --inplace
-py.test test/nbt_test.py
-
